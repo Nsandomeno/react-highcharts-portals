2d1caf81e15b2a043e49ed739bae51e6e2971278
nsandomeno <nsando61@gmail.com>
2020-10-06 11:14:33 -0400

    Rest of the action methods including for multiple stock comparison. Console log some of these response objects and promise chains.

--- src/app/actions/stock-actions.js
+++ src/app/actions/stock-actions.js
@@ -20,3 +20,56 @@ export const getDailyStocksTimeSeries = (symbol) => dispatch => {
         })
     )
 }
+
+export const getWeeklyStocksTimeSeries = (symbol) => dispatch => {
+    fetch(`https://alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${symbol}&apikey=XHMOEBLSQHKEIVE7`).then(
+        response => response.json(),
+        reason => Promise.reject(reason)
+    ).then(
+        stocksData => dispatch({
+            type: STOCK_TIME_SERIES_WEEKLY,
+            payload: stocksData
+        }),
+        reason => dispatch({
+            type: STOCK_TIME_SERIES_WEEKLY,
+            payload: reason
+        })
+    )
+}
+
+export const getMonthlyStocksTimeSeries = (symbol) => dispatch => {
+    fetch(`https://alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=XHMOEBLSQHKEIVE7`).then(
+        response => response.json(), // - Defines successful action
+        reason => Promise.reject(reason) // - Defines error action
+    ).then(
+        stocksData => dispatch({ // - Dispatches successful action
+            type: STOCK_TIME_SERIES_MONTHLY,
+            payload: stocksData
+        }),
+        reason => dispatch({ // - Dispatches error action
+            type: STOCK_TIME_SERIES_MONTHLY,
+            payload: reason
+        })
+    )
+}
+
+export const getMonthlyStocksForComparison = (...symbols) => {
+    // - Promise.all() takes array of promise calls
+    Promise.all(symbols.map(symbol => {
+        fetch(`https://alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=XHMOEBLSQHKEIVE7`).then(
+            response => response.json(),
+            reason => Promise.reject(reason)
+            ).then(
+                stocksData => stocksData,
+                reason => dispatch({
+                    type: MULTIPLE_STOCKS_SERIES_MONTHLY,
+                    payload: reason
+                })
+            )
+    })).then(
+        stocks => dispatch({
+            type: MULTIPLE_STOCKS_SERIES_MONTHLY,
+            payload: stocks
+        })
+    )      
+}
