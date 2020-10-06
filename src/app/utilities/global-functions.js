c6bde4610ec31fb18c063d9767e7b8b4f02be7ca
nsandomeno <nsando61@gmail.com>
2020-10-06 12:08:25 -0400

    Utility function for consuming AlphaVantage API in desired format.

---
+++ src/app/utilities/global-functions.js
@@ -0,0 +1,16 @@
+export default function(timeSeries) {
+    let data = [], stock;
+
+    for (var each in timeSeries) {
+        stock = timeSeries[each];
+
+        data.push([
+            new Date(each).getTime(),
+            parseFloat(stock["1. open"]),
+            parseFloat(stock["2. high"]),
+            parseFloat(stock["3. low"]),
+            parseFloat(stock["4. close"])
+        ]);
+    }
+    return data.reverse();
+}
